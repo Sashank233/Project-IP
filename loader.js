3d696f028ebfd3a6bffa52d718eb5e96022602d2
Maram Rohan Raj <maramrohanraj@gmail.com>
2024-03-06 15:43:41 +0530

    commit

--- loader.js
+++ loader.js
@@ -1,7 +1,7 @@
 let loader = document.getElementById("loader");
 
 document.addEventListener("DOMContentLoaded", () => {
-        loader.style.display = "block";
+        loader.style.display = "none";
 });
 
 
